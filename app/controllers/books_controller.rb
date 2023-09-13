cf42f3e5489162c9c06acfd4ac2a709a1954f018
ryo322 <135493789+ryo322@users.noreply.github.com>
2023-09-13 05:26:20 +0000

    [finish]debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,18 @@
 class BooksController < ApplicationController
+before_action :is_matching_login_user, only: [:edit, :update]
 
+  def new
+    @book = Book.new
+  end 
+  
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -32,15 +39,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+   @book = Book.find(params[:id])
+   unless @book.user.id == current_user.id
+    redirect_to '/books'
+   end
   end
 end
